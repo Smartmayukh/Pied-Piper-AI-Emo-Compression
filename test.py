d05cdfe552ef1c623a15e12c98245faab17d0835
Mayukhmali Das <64318469+Smartmayukh@users.noreply.github.com>
2023-04-07 14:22:36 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,3 @@
+from Pied_Piper_AI_Emo_Compression import main
+
+main.piper_compress("base_audio.wav",31,model_path="C:\\Users\\Mayukhmali Das\\Desktop\\Compress\\Pied_Piper_AI_Emo_Compression\\model",high_bitrate="128k",low_bitrate="64k")
